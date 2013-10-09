d6de52649ece13f391d2f62c0f81bb299e66ad7c
Simon Prosser <simon@pagelines.com>
2013-10-09 22:01:05 +0100

    version bump

--- dms-plugin-pro.php
+++ dms-plugin-pro.php
@@ -4,7 +4,7 @@
 Plugin Name: DMS Professional Tools
 Plugin URI: http://www.pagelines.com/
 Description: Pro member code and utilities for PageLines DMS.
-Version: 1.2
+Version: 1.3
 Author: PageLines
 PageLines: true
 
