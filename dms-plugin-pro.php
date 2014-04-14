79e9095f022372be3b9709f059a877b1e236cc81
Simon Prosser <simon@pagelines.com>
2014-04-14 19:05:23 +0100

    output protocol relative urls

--- dms-plugin-pro.php
+++ dms-plugin-pro.php
@@ -4,7 +4,7 @@
 Plugin Name: DMS Professional Tools
 Plugin URI: http://www.pagelines.com/
 Description: Pro member code and utilities for PageLines DMS.
-Version: 1.5.3
+Version: 1.5.4
 Author: PageLines
 PageLines: true
 
